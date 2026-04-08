e152e0042dfc2d6b224d799c39d8b24b4d1fd1b6
hrutuja864 <hrutujameshram864@gmail.com>
2026-04-08 19:58:58 +0530

    Initial commit - DSA folder

---
+++ Basics/AddFun.java
@@ -0,0 +1,15 @@
+public class AddFun 
+{
+    public static int findSum(int a, int b)
+    {
+        return a+b;
+    }   
+    
+    public static void main(String[] args) 
+    {
+       int num1=5;
+       int num2 =10;
+       int result = findSum(num1,num2);
+       System.out.println("sum = "+result);
+    }
+}
